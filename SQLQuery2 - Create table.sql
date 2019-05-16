71903ba407af02ac9e92117c285de4013339f95f
Henrique Domanski <henriquedomanski@gmail.com>
2019-05-16 17:57:14 -0300

    Aula 7 - Criando Chave Primaria

--- SQLQuery2 - Create table.sql
+++ SQLQuery2 - Create table.sql
@@ -27,8 +27,9 @@ Loja de sucos do Henrique
 
 
 
---Aula 4 - criando base de dados pt1
-CREATE TABLE tb_DadosProduto (
+--Aula 4 - Criando tabelas pt1
+CREATE TABLE tb_Produto (
+	idProduto INT PRIMARY KEY IDENTITY (1,1), --PrimaryKey para identificar produto unico, identity= coxinha N1, SUCOLARANJA N2 etc
 	nmProduto VARCHAR(50) NOT NULL , --obrigatorio, nao recebe valor nulo
 	vlProduto DECIMAL (6,2) NOT NULL --maximo 6 valores e duas virgulas
 
@@ -36,9 +37,10 @@ CREATE TABLE tb_DadosProduto (
 
 
 
---Aula 5- Criando base de dados pt2
+--Aula 5- Criando tabelas pt2
 
 CREATE TABLE tb_Clientes (
+	idCliente INT PRIMARY KEY IDENTITY (1,1)
 	nmCliente VARCHAR (50) NOT NULL , --nome do cliente
 	dtNascimento DATE, -- modelo para data de nascimento
 	tpSexo VARCHAR (1) NOT NULL, --sexo M OU F
@@ -50,6 +52,11 @@ CREATE TABLE tb_Clientes (
 
 
 );
+
+--Aula 6 - teoria banco relacional
+--Aula 7 - Criando Chaves primárias -> Primeira linha Aula4 e 5 IDPRODUTO E IDCLIENTE
+
+
 
 
 
