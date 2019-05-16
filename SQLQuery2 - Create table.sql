ff22186f37a4db97d1cd36d90195c2e8cd222c5a
Henrique Domanski <henriquedomanski@gmail.com>
2019-05-16 18:16:12 -0300

    Aula 8 - Criando Chaves Secundarias

--- SQLQuery2 - Create table.sql
+++ SQLQuery2 - Create table.sql
@@ -28,7 +28,7 @@ Loja de sucos do Henrique
 
 
 --Aula 4 - Criando tabelas pt1
-CREATE TABLE tb_Produto (
+CREATE TABLE tb_Produtos (
 	idProduto INT PRIMARY KEY IDENTITY (1,1), --PrimaryKey para identificar produto unico, identity= coxinha N1, SUCOLARANJA N2 etc
 	nmProduto VARCHAR(50) NOT NULL , --obrigatorio, nao recebe valor nulo
 	vlProduto DECIMAL (6,2) NOT NULL --maximo 6 valores e duas virgulas
@@ -40,7 +40,7 @@ CREATE TABLE tb_Produto (
 --Aula 5- Criando tabelas pt2
 
 CREATE TABLE tb_Clientes (
-	idCliente INT PRIMARY KEY IDENTITY (1,1)
+	idCliente INT PRIMARY KEY IDENTITY (1,1),
 	nmCliente VARCHAR (50) NOT NULL , --nome do cliente
 	dtNascimento DATE, -- modelo para data de nascimento
 	tpSexo VARCHAR (1) NOT NULL, --sexo M OU F
@@ -55,7 +55,22 @@ CREATE TABLE tb_Clientes (
 
 --Aula 6 - teoria banco relacional
 --Aula 7 - Criando Chaves primárias -> Primeira linha Aula4 e 5 IDPRODUTO E IDCLIENTE
+--Aula 8 - Criando chaves secundarias ->
+CREATE TABLE tb_Vendas (
+	idVenda INT PRIMARY KEY IDENTITY (1,1),
+	idProduto INT NOT NULL,
+	vlProduto DECIMAL (6,2) NOT NULL, 
 
+);
+
+
+-- FOREIGN KEY
+-- CONSTRAINT - REGRA, SOMENTE VAI PODER TER O CODIGO DO PRODUTO SE O CODIGO DO PRODUTO JA EXISTIR NA TABELA DE PRODUTOS
+
+ALTER TABLE tb_Vendas -- sempre que for alterar a tabela
+ADD CONSTRAINT FK_Produto_Vendas
+FOREIGN KEY (idProduto)
+REFERENCES tb_Produtos  (idProduto)
 
 
 
