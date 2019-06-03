a01a20d7b1ae952ac78ebb90eed33a07335dd80f
Henrique Domanski <henriquedomanski@gmail.com>
2019-06-03 20:23:32 -0300

    Aula 10 - Select

---
+++ SQLQuery3 - Select.sql
@@ -0,0 +1,12 @@
+--selecionando dados das tabelas
+--SELECT [Colunas] FROM [tabela]
+
+
+
+SELECT nmProduto, idProduto, vlProduto FROM tb_Produtos;
+
+SELECT idProduto FROM tb_Produtos;
+
+
+SELECT idProduto, nmProduto, vlProduto FROM tb_Produtos;
+SELECT * FROM tb_Vendas;
