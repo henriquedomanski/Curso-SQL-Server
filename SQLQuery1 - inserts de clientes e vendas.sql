d3744f5d1675f900ca3885ad943728f08ed0fdc9
Henrique Domanski <henriquedomanski@gmail.com>
2019-07-03 17:49:47 -0300

    Aula 12 - Inserindo cliente e vendas

---
+++ SQLQuery1 - inserts de clientes e vendas.sql
@@ -0,0 +1,17 @@
+-- Inserindo novas consultas
+
+--SELECT * FROM tb_Produtos;
+--SELECT * FROM tb_Vendas;
+--SELECT * FROM tb_Clientes;
+--SELECT * FROM tb_ProdutoVenda;
+
+
+-- 1 - CRIAR CLIENTE:
+--INSERT INTO tb_Clientes VALUES ('Paulo Guina', '1980-05-01', 'M', 'Rua da mecanica, 400', 'campinas', 'Sao Paulo', '11 1234-5678', '11 91011-1213');
+
+
+-- 2 - insert da venda
+--INSERT INTO tb_Vendas VALUES (1, GETDATE());
+
+-- 3 - insert da venda
+--INSERT INTO tb_ProdutoVenda VALUES (1, 1, 3);
