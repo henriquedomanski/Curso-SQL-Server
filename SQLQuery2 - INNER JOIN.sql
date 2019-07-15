3e1a45481369d83b263280700957a3eae2440acb
Henrique Domanski <henriquedomanski@gmail.com>
2019-07-15 12:08:05 -0300

    Aula 14 - Inner join com mais de duas tabelas

--- SQLQuery2 - INNER JOIN.sql
+++ SQLQuery2 - INNER JOIN.sql
@@ -6,6 +6,7 @@ SELECT * FROM tb_Produtos;
 
 -- seleção dos dados com valores
 
-SELECT P.idProduto, P.nmProduto, PV.qtProduto, P.vlProduto  
-FROM tb_Produtos P INNER JOIN tb_ProdutoVenda PV
-ON P.idProduto = PV.idProduto;
+SELECT V.idVenda, C.nmCliente, P.idProduto, P.nmProduto, PV.qtProduto, P.vlProduto  
+FROM tb_Produtos P INNER JOIN tb_ProdutoVenda PV ON P.idProduto = PV.idProduto
+INNER JOIN tb_Vendas V ON PV.idVenda = V.idVenda
+INNER JOIN tb_Clientes C ON V.idCliente = C.idCliente
