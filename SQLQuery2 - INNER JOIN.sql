f9c5b8e23b1ede8f433249a42df8009355d83f4f
Henrique Domanski <henriquedomanski@gmail.com>
2019-07-16 11:53:33 -0300

    Aula16 - como juntar os selecs

--- SQLQuery2 - INNER JOIN.sql
+++ SQLQuery2 - INNER JOIN.sql
@@ -11,19 +11,15 @@ FROM tb_Produtos P INNER JOIN tb_ProdutoVenda PV ON P.idProduto = PV.idProduto
 INNER JOIN tb_Vendas V ON PV.idVenda = V.idVenda
 INNER JOIN tb_Clientes C ON V.idCliente = C.idCliente
 
+--UNIR OS DOIS SELECS
+UNION ALL
+
 -- SOMA DOS VALORES
 
-/*SELECT C.nmCliente, SUM (PV.qtProduto) qtTotalProdutos, p.nmProduto,  SUM (P.vlProduto*PV.qtProduto) --P.vlProduto, qtProduto * vlProduto AS qtValorTotalProduto
+SELECT V.idVenda, C.nmCliente, NULL, 'Total produtos - total vendas', SUM (PV.qtProduto) qtTotalProdutos, NULL,   SUM (P.vlProduto*PV.qtProduto) AS qtValorTotalVenda --P.vlProduto, qtProduto * vlProduto AS qtValorTotalProduto
 FROM tb_Produtos P INNER JOIN tb_ProdutoVenda PV ON P.idProduto = PV.idProduto
 INNER JOIN tb_Vendas V ON PV.idVenda = V.idVenda
 INNER JOIN tb_Clientes C ON V.idCliente = C.idCliente
-GROUP BY C.nmCliente, P.nmProduto*/
-
-
-SELECT C.nmCliente, SUM (PV.qtProduto) qtTotalProdutos,   SUM (P.vlProduto*PV.qtProduto) AS qtValorTotalVenda --P.vlProduto, qtProduto * vlProduto AS qtValorTotalProduto
-FROM tb_Produtos P INNER JOIN tb_ProdutoVenda PV ON P.idProduto = PV.idProduto
-INNER JOIN tb_Vendas V ON PV.idVenda = V.idVenda
-INNER JOIN tb_Clientes C ON V.idCliente = C.idCliente
-GROUP BY C.nmCliente
+GROUP BY V.idVenda, C.nmCliente
 
 
