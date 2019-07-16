a69aaafe449a64ae4db5c1413a2fe31ae1434692
Henrique Domanski <henriquedomanski@gmail.com>
2019-07-16 12:10:20 -0300

    Aula 17 - SubSelecst

--- SQLQuery2 - INNER JOIN.sql
+++ SQLQuery2 - INNER JOIN.sql
@@ -5,21 +5,15 @@ SELECT * FROM tb_ProdutoVenda;
 SELECT * FROM tb_Produtos;
 
 -- seleção dos dados com valores
-
-SELECT V.idVenda, C.nmCliente, P.idProduto, P.nmProduto, PV.qtProduto, P.vlProduto, qtProduto * vlProduto AS qtValorTotalProduto
+SELECT V.idVenda, C.nmCliente, P.idProduto, P.nmProduto, PV.qtProduto, P.vlProduto, qtProduto * vlProduto AS qtValorTotalProduto, 
+TOTAL_VENDAS.qtTotalProdutos, TOTAL_VENDAS.qtValorTotalVenda
 FROM tb_Produtos P INNER JOIN tb_ProdutoVenda PV ON P.idProduto = PV.idProduto
 INNER JOIN tb_Vendas V ON PV.idVenda = V.idVenda
 INNER JOIN tb_Clientes C ON V.idCliente = C.idCliente
+INNER JOIN (
+	SELECT V.idVenda, SUM (PV.qtProduto) qtTotalProdutos, SUM (P.vlProduto*PV.qtProduto) AS qtValorTotalVenda --P.vlProduto, qtProduto * vlProduto AS qtValorTotalProduto
+	FROM tb_Produtos P INNER JOIN tb_ProdutoVenda PV ON P.idProduto = PV.idProduto
+	INNER JOIN tb_Vendas V ON PV.idVenda = V.idVenda
+	GROUP BY V.idVenda
 
---UNIR OS DOIS SELECS
-UNION ALL
-
--- SOMA DOS VALORES
-
-SELECT V.idVenda, C.nmCliente, NULL, 'Total produtos - total vendas', SUM (PV.qtProduto) qtTotalProdutos, NULL,   SUM (P.vlProduto*PV.qtProduto) AS qtValorTotalVenda --P.vlProduto, qtProduto * vlProduto AS qtValorTotalProduto
-FROM tb_Produtos P INNER JOIN tb_ProdutoVenda PV ON P.idProduto = PV.idProduto
-INNER JOIN tb_Vendas V ON PV.idVenda = V.idVenda
-INNER JOIN tb_Clientes C ON V.idCliente = C.idCliente
-GROUP BY V.idVenda, C.nmCliente
-
-
+)TOTAL_VENDAS ON V.idVenda = TOTAL_VENDAS.idVenda
