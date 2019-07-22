b2e307a486c4709bada17e7e59a444b234983337
Henrique Domanski <henriquedomanski@gmail.com>
2019-07-22 15:24:00 -0300

    Aula 21 - Criando Constraints Para As Novas Tabelas

--- SQLQuery1- Criando novas tabelas pt1.sql
+++ SQLQuery1- Criando novas tabelas pt1.sql
@@ -1,23 +1,36 @@
-CREATE TABLE tb_Endereco {
-	idEndereco INT NOTNULL PRIMARY KEY IDENTITY (1,1),
+CREATE TABLE tb_Endereco (
+	idEndereco INT NOT NULL PRIMARY KEY IDENTITY (1,1),
 	nmEndereco VARCHAR (50) NOT NULL,
-	nmCEP VARCHAR (9)
+	nmCEP VARCHAR (9),
+	idCidade INT NOT NULL
 
-}
+)
 
 
-CREATE TABLE tb_Cidades {
+CREATE TABLE tb_Cidades (
 	idCidade INT NOT NULL PRIMARY KEY IDENTITY (1,1),
-	nmCidade VARCHAR (50) NOT NULL
+	nmCidade VARCHAR (50) NOT NULL,
+	idEstado INT NOT NULL
+
+)
 
 
-}
-
-
-CREATE TABLE tb_Estados {
+CREATE TABLE tb_Estados (
 	idEstados INT NOT NULL PRIMARY KEY IDENTITY (1,1),
 	nmEstado VARCHAR(30) NOT NULL,
 	nmSigla VARCHAR (2) NOT NULL
 
 
-}
+)
+
+
+
+ALTER TABLE tb_Enderecos
+ADD CONSTRAINT FK_Enderecos_Cidades
+FOREIGN KEY (idCidade)
+REFERENCES tb_Cidades (idCidade);
+
+ALTER TABLE tb_Cidades
+ADD CONSTRAINT FK_Cidades_Estados
+FOREIGN KEY (idEstado)
+REFERENCES tb_Estados (idEstado);
