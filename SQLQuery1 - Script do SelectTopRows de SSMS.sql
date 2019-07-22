aaa44388689f4198aa320f5f701993a97296eaad
Henrique Domanski <henriquedomanski@gmail.com>
2019-07-22 16:14:23 -0300

    Aula 23 - Transações, BEGIN TRAN E ROLLBACK

--- SQLQuery1 - Script do SelectTopRows de SSMS.sql
+++ SQLQuery1 - Script do SelectTopRows de SSMS.sql
@@ -1,5 +1,5 @@
 /* Script do comando SelectTopRows de SSMS */
-SELECT TOP 1000 [idCliente]
+/*SELECT TOP 1000 [idCliente]
 		,[nmCliente]
 		,[dtNascimento]
 		,[tpSexo]
@@ -9,27 +9,57 @@ SELECT TOP 1000 [idCliente]
 		,[nmTelefone1]
 		,[nmTelefone2]
 	FROM [dbLojaHenrique].[dbo].[tb_ClientesBackup]
+	*/
 
+	-- Iniciar a transação
+BEGIN TRAN
 
 	CREATE TABLE #tmp_Estados (
+		idEstado INT IDENTITY (1,1),
 		nmEstado VARCHAR (30),
 		nmSigla VARCHAR (2)
 	
 	);
 
 	
-
+	
 
 -- inserir os dados dos estados na tabela temporaria
 
 INSERT INTO #tmp_Estados (nmEstado)
-SELECT DISTINCT nmEstado FROM tb_ClientesBackup
+SELECT DISTINCT nmEstado FROM tb_Clientes ORDER BY nmEstado;
+
+-- tratamento dos dados
+
+
+
+-- 
 
 
 SELECT * FROM #tmp_Estados
+
+
 
 -- update na sigla
 
 UPDATE #tmp_Estados SET nmSigla = 'SP' WHERE nmEstado =  'São Paulo';
 
---DELETE FROM #tmp_Estados WHERE nmEstado = 'São Paulo';
+SET IDENTITY_INSERT tb_Estados ON; -- INSERE na coluna de identity
+
+-- inserindo na tabela de estado
+
+INSERT INTO tb_Estados (idEstado, nmEstado, nmSigla)
+SELECT idEstado, nmEstado, nmSigla FROM #tmp_Estados;
+
+-- SELECT  * FROM #tmp_Estado
+-- SELECT * FROM tb_Estados;
+
+--DELETE FROM #tmp_Estados WHERE nmEstado = 'Sao Paulo';
+
+
+SET IDENTITY_INSERT tb_Estados OFF; 
+
+
+
+ROLLBACK TRAN; -- remove tudo que está apartir do begin tran
+--COMMIT TRAN;
