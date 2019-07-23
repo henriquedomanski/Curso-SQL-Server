3740b9699ce6a8898c7f8aae6fbe55fb07317908
henriquedomanski <henrydomanski@gmail.com>
2019-07-23 18:25:09 -0300

    Aula 25 - Update com Join

--- SQLQuery1 - Migranda dados para a tabela de cidades.sql
+++ SQLQuery1 - Migranda dados para a tabela de cidades.sql
@@ -16,9 +16,24 @@ SELECT DISTINCT nmCidade FROM tb_Clientes ORDER BY nmCidade;
 -- tratamento de dados
 
 -- UPDATE DA SIGLA DO ESTADO
-SELECT * FROM tb_Estados  E 
+
+--mais complicado (update com join)
+BEGIN TRAN;
+
+UPDATE TC 
+SET TC.idEstado = E.idEstado 
+FROM #tmp_Cidades TC
+INNER JOIN tb_Clientes T ON TC.nmCidade = T.nmCidade
+INNER JOIN tb_Estados E ON T.nmEstado = E.nmEstado
+
+
+
+SELECT DISTINCT E.idEstado, C.nmCidade FROM tb_Estados  E 
 INNER JOIN tb_Clientes C ON E.nmEstado = C.nmEstado 
 
+
+
+--SELECT * FROM #tmp_Cidades
 ROLLBACK TRAN;
 --COMMIT TRAN;
 
