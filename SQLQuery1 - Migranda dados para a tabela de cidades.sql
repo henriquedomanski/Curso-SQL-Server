397843f9ab464f3547974e5f26d97303b8b0330f
henriquedomanski <henrydomanski@gmail.com>
2019-07-24 11:07:44 -0300

    Aula 26 - Migrando dados para a tabela de cidades (Parte 2)

--- SQLQuery1 - Migranda dados para a tabela de cidades.sql
+++ SQLQuery1 - Migranda dados para a tabela de cidades.sql
@@ -5,13 +5,15 @@ BEGIN TRAN; -- Iniciar a transação
 CREATE TABLE #tmp_Cidades (
 	idCidade INT IDENTITY (1,1),
 	nmCidade VARCHAR (50),
+	nmEstado VARCHAR (30),
 	idEstado INT
 );
 
+
 --Inserir os dados das cidades na tabela temp
 
-INSERT INTO #tmp_Cidades (nmCidade)
-SELECT DISTINCT nmCidade FROM tb_Clientes ORDER BY nmCidade;
+INSERT INTO #tmp_Cidades (nmCidade, nmEstado)
+SELECT DISTINCT nmCidade, nmEstado FROM tb_Clientes ORDER BY nmCidade;
 
 -- tratamento de dados
 
@@ -20,20 +22,40 @@ SELECT DISTINCT nmCidade FROM tb_Clientes ORDER BY nmCidade;
 --mais complicado (update com join)
 BEGIN TRAN;
 
+/*
 UPDATE TC 
 SET TC.idEstado = E.idEstado 
 FROM #tmp_Cidades TC
 INNER JOIN tb_Clientes T ON TC.nmCidade = T.nmCidade
 INNER JOIN tb_Estados E ON T.nmEstado = E.nmEstado
 
+*/
 
+--Mais facil:
 
-SELECT DISTINCT E.idEstado, C.nmCidade FROM tb_Estados  E 
-INNER JOIN tb_Clientes C ON E.nmEstado = C.nmEstado 
-
+UPDATE TC SET
+TC.idEstado = E.idEstado 
+FROM #tmp_Cidades TC INNER JOIN tb_Estados E
+ON TC.nmEstado = E.nmEstado;
 
 
 --SELECT * FROM #tmp_Cidades
+
+SET IDENTITY_INSERT tb_Cidades ON; -- INSERE na coluna de identity
+
+-- Inserir dados na tabela
+
+INSERT INTO tb_Cidades (idCidade, nmCidade, idEstado)
+SELECT idCidade, nmCidade, idEstado FROM #tmp_Cidades;
+
+--SELECT * FROM tb_Cidades
+
+DROP TABLE #tmp_Cidades
+
+
+
+SET IDENTITY_INSERT tb_Cidades OFF;
+
 ROLLBACK TRAN;
 --COMMIT TRAN;
 
