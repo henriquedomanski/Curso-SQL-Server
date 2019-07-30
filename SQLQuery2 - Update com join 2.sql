30aa16274948d6b24b77611d38fa09de2169789d
henriquedomanski <henrydomanski@gmail.com>
2019-07-30 19:21:33 -0300

    Aula 30 - Update com Join 2

---
+++ SQLQuery2 - Update com join 2.sql
@@ -0,0 +1,12 @@
+/*BEGIN TRAN; UPDATE C SET
+C.idEndereco = E.idEndereco
+FROM tb_Enderecos E
+INNER JOIN tb_Clientes C ON E.nmEndereco = C.nmEndereco
+
+
+SELECT * FROM tb_Clientes;
+
+SELECT * FROM tb_Enderecos;
+
+COMMIT TRAN;*/
+
