5a75c568d3f1eb348bd9ed5d5fdb260b95b28908
henriquedomanski <henrydomanski@gmail.com>
2019-07-30 19:46:18 -0300

    Aula 32 - Criando View

---
+++ SQLQuery4 - Criando View.sql
@@ -0,0 +1,21 @@
+CREATE VIEW vw_Endereco_Completo_Clientes
+AS
+SELECT 
+	CL.idCliente,
+	CL.nmCliente,
+	CL.dtNascimento,
+	CL.tpSexo,
+	EN.idEndereco,
+	EN.nmEndereco,
+	EN.nmCEP,
+	CI.idCidade,
+	CI.nmCidade,
+	E.idEstado,
+	E.nmEstado,
+	E.nmSigla
+FROM tb_Clientes CL INNER JOIN tb_Enderecos EN ON CL.idEndereco = EN.idEndereco
+INNER JOIN tb_Cidades CI ON EN.idCidade = CI.idCidade
+INNER JOIN tb_Estados E ON CI.idEstado = E.idEstado;
+
+SELECT * FROM vw_Endereco_Completo_Clientes;
+
