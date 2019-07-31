8823e076045d37d47f93ca38d7ad2f3d20604929
henriquedomanski <henrydomanski@gmail.com>
2019-07-31 10:39:17 -0300

    Aula 37 - Criando Trigger

--- SQLQuery5 - Refatorando tabela telefones.sql
+++ SQLQuery5 - Refatorando tabela telefones.sql
@@ -1,4 +1,4 @@
---DROP TABLE tb_ClientesBackup;
+/*DROP TABLE tb_ClientesBackup;
 
 CREATE TABLE tb_Clientes_Hist (
 	idClientes_Hist INT NOT NULL IDENTITY (1,1) PRIMARY KEY,
@@ -6,7 +6,22 @@ CREATE TABLE tb_Clientes_Hist (
 	nmCliente VARCHAR (50),
 	dtNascimento DATE NULL,
 	tpSexo VARCHAR (1) NULL,
-	nmTelefone1 
+	idEndereco INT NULL,
+	dtInclusao DATETIME
 
-);
+);*/
 
+/*CREATE TRIGGER tr_Backup_Clientes
+ON tb_Clientes
+FOR UPDATE 
+AS
+	INSERT INTO tb_Clientes_Hist (idCliente, nmCliente,  dtNascimento, tpSexo, idEndereco, dtInclusao) 
+	SELECT  idCliente, nmCliente,  dtNascimento, tpSexo, idEndereco, GETDATE() FROM deleted;*/
+
+
+SELECT * FROM tb_Clientes
+SELECT * FROM tb_Clientes_Hist;
+
+BEGIN TRAN; UPDATE tb_Clientes SET nmCliente = 'Jailson' WHERE idCliente = 1; 
+
+COMMIT TRAN;
