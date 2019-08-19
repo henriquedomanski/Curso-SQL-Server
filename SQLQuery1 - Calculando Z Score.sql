6aa0a3eafcd05fe18c0b0d117185dc654213c6c5
henriquedomanski <henrydomanski@gmail.com>
2019-08-19 10:52:42 -0300

    Aula 6 - Usando OVER

--- SQLQuery1 - Calculando Z Score.sql
+++ SQLQuery1 - Calculando Z Score.sql
@@ -1,12 +1,24 @@
-SELECT AVG(vlProduto) FROM tb_Produtos--media
+/*SELECT AVG(vlProduto) FROM tb_Produtos--media
 
 SELECT STDEV (vlProduto) FROM tb_Produtos --desvio padrao
-
+*/
 
 -- Z Score é o valor que estou verificando menos a media mostral dividido pelo desvio padrão
 --Calcular Z_Score:
-SELECT  nmProduto, vlProduto, Calc.Media, Calc.DesvioPadrao, (vlProduto - Calc.Media) / Calc.DesvioPadrao as Z_Score
+
+/*SELECT  nmProduto, vlProduto, Calc.Media, Calc.DesvioPadrao, (vlProduto - Calc.Media) / Calc.DesvioPadrao as Z_Score
 FROM tb_Produtos, (
 SELECT  AVG (vlProduto) as Media,  
 STDEV (vlProduto) as DesvioPadrao
-FROM tb_Produtos) Calc
+FROM tb_Produtos) Calc*/
+
+/*
+SELECT nmProduto, vlProduto,
+	AVG (vlProduto) OVER() as Media,
+	STDEV (vlProduto) OVER() as DesvioPadrao
+	
+FROM
+	tb_Produtos
+*/
+
+
